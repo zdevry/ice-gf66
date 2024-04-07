4d3bfb21135a88146275d664d626f20191efbd79
zijkdefry <tanivan2u@gmail.com>
2024-04-07 23:29:39 +0800

    scaffold single file scripts

--- README.md
+++ README.md
@@ -3,8 +3,7 @@
 Currently doing a total rewrite on this branch.
 - [ ] Totally changing the interface to use TOML (or JSON?)
 files for configuration and profiles
-- [ ] Structuring the program better (at the very least separating
-the CLI logic from the backend logic)
+- [ ] Putting everything in one file? (idk, and another(?))
 - [ ] Rewriting the NVIDIA module to use NVML so that it works on Wayland
 (or rather does not require Xorg ran as root to set clock offsets)
 - [ ] Manually writing to MSR registers to remove msr-tools dependency
--- src/modules/nvidia.py
+++ src/modules/nvidia.py
@@ -1,40 +1,27 @@
-from subprocess import run
+import ctypes
 
-persistence_mode = False # Assumed
+class NVMLError(Exception): pass
 
-def check_persistence_mode(dry):
-    global persistence_mode
-    if persistence_mode: return
-    
-    print('\x1b[1;93mEnabling persistence mode...\x1b[0m')
+def load_nvml(libpath):
+    return ctypes.cdll.LoadLibrary(libpath)
 
-    persistence_mode = True
+def check_error(code):
+    if code != 0:
+        raise NVMLError(f'NVML error code {code}')
 
-    if dry:
-        print('\x1b[1;94mexec:\x1b[0m '
-            'nvidia-smi -pm 1')
-        return
-    
-    run(['nvidia-smi', '-pm', '1'])
+def initialise(libnvml):
+    check_error(libnvml.nvmlInit_v2())
 
-def set_clock_limit(limit, dry):
-    check_persistence_mode(dry)
-    print(f'\x1b[1;93mSetting GPU clock limit to {limit} MHz...\x1b[0m')
-    
-    if dry:
-        print('\x1b[1;94mexec:\x1b[0m '
-            f'nvidia-smi -lgc {limit}')
-        return
-    
-    run(['nvidia-smi', '-lgc', limit])
+def shutdown(libnvml):
+    check_error(libnvml.nvmlShutdown())
 
-def set_clock_offset(offset, dry):
-    check_persistence_mode(dry)
-    print(f'\x1b[1;93mSetting GPU clock offset to +{offset}MHz...\x1b[0m')
-    
-    if dry:
-        print('\x1b[1;94mexec:\x1b[0m '
-            f'nvidia-settings -a [gpu:0]/GPUGraphicsClockOffsetAllPerformanceLevels={offset}')
-        return
-    
-    run(['nvidia-settings', '-a', f'[gpu:0]/GPUGraphicsClockOffsetAllPerformanceLevels={offset}'])
+def get_device_handle_by_index(libnvml, index):
+    device = ctypes.c_void_p(0)
+    pdev = ctypes.pointer(device)
+
+    check_error(libnvml.nvmlDeviceGetHandleByIndex_v2(index, pdev))
+
+    return device.value
+
+def set_device_clock_offset(libnvml, handle, offset):
+    check_error(libnvml.nvmlDeviceSetGpcClkVfOffset(ctypes.c_void_p(handle), ctypes.c_int(offset)))

09cde041aba7e38697c355a8bde407a9dfdae18c
zijkdefry <tanivan2u@gmail.com>
2024-04-07 18:19:27 +0800

    scaffold intel msr module

--- src/modules/intel.py
+++ src/modules/intel.py
@@ -1,104 +1,49 @@
-from subprocess import run
+MSR_FILE_FORMAT = '/dev/cpu/{}/msr'
 
-REGISTER_UNDERVOLT = '0x150'
+TURBO_REGISTER = 0x1AD
+UNDERVOLT_REGISTER = 0x150
 
-CPU_DIGIT = '0'
-CACHE_DIGIT = '2'
+READ_BYTE = b'\x10'
+WRITE_BYTE = b'\x11'
 
-READ_DIGIT = '0'
-WRITE_DIGIT = '1'
-
-READ_CPU_UVOLT_ARG = f'0x80000{CPU_DIGIT}1{READ_DIGIT}00000000'
-READ_CACHE_UVOLT_ARG = f'0x80000{CACHE_DIGIT}1{READ_DIGIT}00000000'
-
-WRITE_CPU_UVOLT_PREFIX = f'0x80000{CPU_DIGIT}1{WRITE_DIGIT}'
-WRITE_CACHE_UVOLT_PREFIX = f'0x80000{CACHE_DIGIT}1{WRITE_DIGIT}'
-
-REGISTER_TURBO_BOOST = '0x1ad'
-
-def read_turbo_boost(dry):
-    print('\x1b[1;93mCurrent Turbo Boost ratios:\x1b[0m')
-
-    if dry:
-        print(f'\x1b[1;94mexec:\x1b[0m '
-            f'rdmsr {REGISTER_TURBO_BOOST} '
-            f'\x1b[1;95m(read as array of turbo boost ratios)\x1b[0m')
-        return
-    
-    read_output = run(['rdmsr', REGISTER_TURBO_BOOST], capture_output=True)
-
-    turbo_boosts = [ int(read_output.stdout[i:i+2], 16) for i in range(0, 16, 2) ]
-    
-    for cores, ratio in enumerate(reversed(turbo_boosts), 1):
-        print(f'{cores} core: {ratio} ({ratio * 100}MHz)')
+UVOLT_PLANE_BYTES = {
+    'core': b'\x00',
+    'igpu': b'\x01',
+    'cache': b'\x02',
+    'agent': b'\x03',
+    'analog_io': b'\x04',
+    'digital_io': b'\x05',
+}
 
 
-def set_turbo_boost(ratios, dry):
-    set_turbo_boost_arg = '0x' + (''.join([ f'{r:02x}' for r in ratios ]))
-    
-    turbo_ratio_msg = ','.join(map(str, ratios))
-    print(f'\x1b[1;93mApplying Turbo Boost Ratios {turbo_ratio_msg}...\x1b[0m')
+def wrmsr_on_all_cpus(reg, regval, num_cores):
+    for i in range(num_cores):
+        wrmsr_on_cpu(reg, i, regval)
 
-    if dry:
-        print(f'\x1b[1;94mexec:\x1b[0m '
-            f'wrmsr {REGISTER_TURBO_BOOST} {set_turbo_boost_arg}')
-        return
-    
-    run(['wrmsr', REGISTER_TURBO_BOOST, set_turbo_boost_arg])
+def wrmsr_on_cpu(reg, cpu, regval):
+    file = MSR_FILE_FORMAT.format(cpu)
+    wrnum = int.from_bytes(regval, byteorder='little')
+    print(f'Write {regval} (0x{wrnum:016x}) to register {reg:04x} : {file}')
 
 
-def read_plane_undervolt(plane_arg, plane_name, dry):
-    if dry:
-        print(f'\x1b[1;94mexec:\x1b[0m '
-            f'wrmsr {REGISTER_UNDERVOLT} {plane_arg}')
-        print(f'\x1b[1;94mexec:\x1b[0m '
-            f'rdmsr {REGISTER_UNDERVOLT} '
-            f'\x1b[1;95m(read as {plane_name} undervolt)\x1b[0m')
-        return None
-    
-    run(['wrmsr', REGISTER_UNDERVOLT, plane_arg])
-    read_undervolt = run(['rdmsr', REGISTER_UNDERVOLT], capture_output=True)
+def write_turbo_boosts(boosts, num_cores):
+    tailing_boosts = 8 - len(boosts)
+    all_boosts = boosts + boosts[-1:-2:-1] * tailing_boosts
 
-    uvolt = int(read_undervolt.stdout, 16)
-    return ((uvolt >> 21) ^ 0x7ff) + 1
+    wrmsr_on_all_cpus(TURBO_REGISTER, bytes(all_boosts), num_cores)
 
 
-def read_cpu_uvolt(dry):
-    return read_plane_undervolt(READ_CPU_UVOLT_ARG, 'CPU', dry)
+def undervolt_bytes(uvolt):
+    b = (-uvolt & 0x7FF) << 21
+    return b.to_bytes(4, byteorder='little')
 
-def read_cache_uvolt(dry):
-    return read_plane_undervolt(READ_CPU_UVOLT_ARG, 'cache', dry)
+def write_undervolt(uvolts, num_cores):
+    for plane in uvolts:
+        if plane not in UVOLT_PLANE_BYTES:
+            continue
+        plane_byte = UVOLT_PLANE_BYTES[plane]
+        uvolt = uvolts[plane]
+        
+        regval = undervolt_bytes(uvolt) + WRITE_BYTE + plane_byte + b'\x00\x80'
 
-
-def read_undervolt(dry):
-    print('\x1b[1;93mCurrent CPU undervolt:\x1b[0m')
-
-    cpu_uvolt = read_cpu_uvolt(dry)
-    cache_uvolt = read_cache_uvolt(dry)
-
-    if dry: return
-    
-    print(f'CPU: {cpu_uvolt} (-{cpu_uvolt/1.024:.1f}mV)')
-    print(f'Cache: {cache_uvolt} (-{cache_uvolt/1.024:.1f}mV)')
-
-
-def offset_bytes(offset):
-    b = (-offset & 0x7ff) << 21
-    return f'{b:08x}'
-
-def undervolt(offset, dry):
-    uvolt_offset = offset_bytes(offset)
-    cpu_uvolt_bytes = f'{WRITE_CPU_UVOLT_PREFIX}{uvolt_offset}'
-    cache_uvolt_bytes = f'{WRITE_CACHE_UVOLT_PREFIX}{uvolt_offset}'
-
-    print(f'\x1b[1;93mApplying CPU undervolt of {offset} units...\x1b[0m')
-
-    if dry:
-        print(f'\x1b[1;94mexec:\x1b[0m '
-            f'wrmsr {REGISTER_UNDERVOLT} {cpu_uvolt_bytes}')
-        print(f'\x1b[1;94mexec:\x1b[0m '
-            f'wrmsr {REGISTER_UNDERVOLT} {cache_uvolt_bytes}')
-        return
-    
-    run(['wrmsr', REGISTER_UNDERVOLT, cpu_uvolt_bytes])
-    run(['wrmsr', REGISTER_UNDERVOLT, cache_uvolt_bytes])
+        wrmsr_on_all_cpus(UNDERVOLT_REGISTER, regval, num_cores)
--- src/profiles.py
+++ src/profiles.py
@@ -1,11 +1,7 @@
-import tomllib
 import modules.msi_ec
+import modules.intel
 
-def write_msi_ec_profile(prof):
-    f_conf = open('msi-ec.toml', 'rb')
-    conf = tomllib.load(f_conf)
-    f_conf.close()
-
+def write_msi_ec_profile(prof, conf):
     ec = modules.msi_ec.open_ec_file(conf)
     try:
         if 'shift' in prof:
@@ -16,6 +12,14 @@ def write_msi_ec_profile(prof):
         # ec.close()
         pass
 
-def write_profile(prof):
+def write_intel_profile(prof, num_cores):
+    if 'undervolt' in prof:
+        modules.intel.write_undervolt(prof['undervolt'], num_cores)
+    if 'turbo' in prof:
+        modules.intel.write_turbo_boosts(prof['turbo'], num_cores)
+
+def write_profile(prof, conf):
     if 'msi_ec' in prof:
-        write_msi_ec_profile(prof['msi_ec'])
+        write_msi_ec_profile(prof['msi_ec'], conf['ec'])
+    if 'intel' in prof:
+        write_intel_profile(prof['intel'], conf['msr']['num_cores'])
